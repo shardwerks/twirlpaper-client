eb2bfca817ef8df7c00e748107e5c277e9ce42f4
shard <none@none>
2010-01-06 13:44:22 -0800

    Add ico to exe

--- build_clean.bat
+++
@@ -1,4 +0,0 @@
-del /q *.pyc
-del /q *.bak
-del /q Exe\build
-del /q Exe\dist
--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from distutils.core import setup
 import py2exe
 
-setup(options = {"py2exe": {"dll_excludes": ["w9xpopen.exe", "MSVCP90.dll"],
-							"bundle_files": 1}},
-	windows=["twirlpaper.py"],
+setup(options = {"py2exe" : {"dll_excludes" : ["w9xpopen.exe", "MSVCP90.dll"],
+							"compressed" : 1, "optimize" : 2, "bundle_files" : 1}
+				},
+	windows=[{"script" : "twirlpaper.py",
+				"icon_resources": [(1, "twirldesktop.ico")]
+			}],
 	zipfile = None
 )
